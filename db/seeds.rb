04fc93ca5e2dd2bfd5507faed52cf91de82b95f4
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-12-14 03:06:41 +0100

    a lil front work

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,31 @@ Flat.create!(
   price_per_night: 65,
   number_of_guests: 3
 )
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Incredible view of Paris',
+  address: '89 rue de Lille',
+  description: 'Just get it',
+  price_per_night: 125,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Hot Damn',
+  address: '10 rue de lu pu Paris',
+  description: 'Just like that',
+  price_per_night: 95,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Ayo Chicagoooo',
+  address: '10 Clifton Bitch Chicago',
+  description: "they don't call it the windy for no reason",
+  price_per_night: 65,
+  number_of_guests: 3
+)
