0ca0e50a4e27ab9d3a500a7e69498b4aca3ce75e
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-12-14 03:13:18 +0100

    front after new seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_000645) do
+ActiveRecord::Schema.define(version: 2021_12_14_020947) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_14_000645) do
     t.integer "number_of_guests"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture_url"
   end
 
 end
